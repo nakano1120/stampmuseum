fb1fa8c83f16860cfba497d29f0a3647bf5c66a0
nakano1120 <38690180+nakano1120@users.noreply.github.com>
2021-03-19 15:28:17 +0900

    ADD 138

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 window.onload=function(){
-    for(let num=1;num<=134;num++){
+    for(let num=1;num<=138;num++){
         nownum = ( '0000' + num ).slice( -5 );
         let nowdiv = "<div class='lefter'><img src='stamp/nakano-"+nownum+".png'></div>"
         document.getElementById("container").innerHTML+=nowdiv
