8320c0a7561f13c968e6a0f867fed9ca7614a413
nakano1120 <38690180+nakano1120@users.noreply.github.com>
2021-05-30 20:54:10 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 window.onload=function(){
-    for(let num=1;num<=147;num++){
+    for(let num=1;num<=165;num++){
         nownum = ( '0000' + num ).slice( -5 );
         let nowdiv = "<div class='lefter'><img src='stamp/nakano-"+nownum+".png'></div>"
         document.getElementById("container").innerHTML+=nowdiv
